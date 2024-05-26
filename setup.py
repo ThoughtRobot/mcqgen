3e6b3fd5267c66435b4a16cffb29454090126a7f
ThoughtRobot <raamish2003@gmail.com>
2024-05-26 19:40:58 +0530

    files updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author="Raamish Laaraib",
+    author_email="raamish2003@gmail.com",
+    packages=find_packages()
+)
